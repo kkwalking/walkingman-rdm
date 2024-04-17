8ea598127c9a1a5280fe3423fe13bfb3b6043f15
zhouzekun <zhouzk3@chinatelecom.cn>
2024-04-17 17:30:37 +0800

    add LICENSE

---
+++ src/main/java/com/kelton/kkwalkingrdm/RdmBootApplication.java
@@ -0,0 +1,11 @@
+package com.kelton.kkwalkingrdm;
+
+/**
+ * @Author zhouzekun
+ * @Date 2024/4/17 17:27
+ */
+public class RdmBootApplication {
+    public static void main(String[] args) {
+        System.out.println("kkwalking-rdm");
+    }
+}
