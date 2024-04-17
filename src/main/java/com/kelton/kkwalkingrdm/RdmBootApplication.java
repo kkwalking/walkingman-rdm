e3257948e59419813f8ca14248519c6a54ef4d9c
Kelton <417160807@qq.com>
2024-04-17 23:42:31 +0800

    add log module

--- src/main/java/com/kelton/kkwalkingrdm/RdmBootApplication.java
+++ src/main/java/com/kelton/kkwalkingrdm/RdmBootApplication.java
@@ -1,9 +1,12 @@
 package com.kelton.kkwalkingrdm;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @Author zhouzekun
  * @Date 2024/4/17 17:27
  */
+@Slf4j
 public class RdmBootApplication {
     public static void main(String[] args) {
         System.out.println("kkwalking-rdm");
